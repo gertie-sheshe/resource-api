87a1fd83ed252eb784b15c1202e28fb3be828b7b
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-08-13 15:12:10 +0300

    (feat) add update + delete crud

--- src/controllers/v0/users/routes/user.routes.js
+++ src/controllers/v0/users/routes/user.routes.js
@@ -3,6 +3,12 @@ import controllers from "../users.controllers.js";
 
 const router = Router();
 
-router.route("/").post(controllers.createOne);
+router.route("/").post(controllers.createOne).get(controllers.getAll);
+
+router
+  .route("/:id")
+  .get(controllers.getOne)
+  .delete(controllers.removeOne)
+  .put(controllers.updateOne);
 
 export const UsersRouter = router;
--- src/utils/crud.js
+++ src/utils/crud.js
@@ -15,7 +15,6 @@ export const getOne = (model) => async (req, res) => {
 
 export const createOne = (model) => async (req, res) => {
   try {
-    // const doc = model.create({ ...req.body });
     const data = new model(req.body);
     const doc = await data.save();
     return res.status(201).json({ data: doc });
@@ -25,7 +24,59 @@ export const createOne = (model) => async (req, res) => {
   }
 };
 
+export const removeOne = (model) => async (req, res) => {
+  try {
+    const removedDoc = await model.findOneAndRemove({ _id: req.params.id });
+
+    if (!removedDoc) {
+      return res.status(400).end();
+    }
+
+    return res.status(200).json({ data: removedDoc });
+  } catch (e) {
+    console.error(e);
+    res.status(400).end();
+  }
+};
+
+export const updateOne = (model) => async (req, res) => {
+  try {
+    const updatedDoc = await model
+      .findOneAndUpdate(
+        {
+          _id: req.params.id,
+        },
+        req.body,
+        { new: true }
+      )
+      .lean()
+      .exec();
+
+    if (!updatedDoc) {
+      return res.status(400).end();
+    }
+
+    return res.status(200).json({ data: updatedDoc });
+  } catch (e) {
+    console.error(e);
+    res.status(400).end();
+  }
+};
+
+export const getAll = (model) => async (req, res) => {
+  try {
+    const documents = await model.find({}).lean().exec();
+    return res.status(200).json({ data: documents });
+  } catch (e) {
+    console.error(e);
+    res.status(400).end();
+  }
+};
+
 export const crudControllers = (model) => ({
   getOne: getOne(model),
   createOne: createOne(model),
+  removeOne: removeOne(model),
+  updateOne: updateOne(model),
+  getAll: getAll(model),
 });
